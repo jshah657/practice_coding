56183f2af92268d470cfb017d6c4914bc94b7701
Jay Shah <jayshah.11292@gmail.com>
2024-01-01 16:58:55 -0500

    new code changes

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-
+#Let's start basic: How do you comment your code? Its by adding a hashtag in the front of your sentence or word. Like this one. 
+#print hello world
+print("Hello World")
+# Assign a value to a varibale. What is variable? Think of a variable as a storage for your assigned value that you can use throught the coding. For example: 
+name = {"first name" : "Jay",
+        "last name" : "Shah",
+        "age" : 31,
+        "spouse name" : "Heemali Patel"}
+print(name)
+# As you can see that the above variable has multiple values which are fortmatted using dictionary. Let's print first name and last name. 
+print(name['first name'] + " " + name["last name"])
