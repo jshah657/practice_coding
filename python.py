ed7ce054f113892e1d1eae2d45cf6231b170404c
Jay Shah <Jayshah.11292@gmail.com>
2024-02-01 16:54:10 -0500

    adding more code

--- python.py
+++ python.py
@@ -17,3 +17,15 @@ print(name['first name'] + " " + name["last name"])
 numbers = [1,2,3,4,5,6,7,8,9,10]
 print(numbers[0]) #as you can see that [0] is the first value in the array. 
 print(numbers[0:2]) #this should print 1,2
+
+# Let's understand errors in Python code. 
+# Runtime errors = RuntimeError are caught in Python code that is not defined. 
+ 
+# Let's understand variables. Following variables are defined to store values in variables. 
+firstName = "Jay"
+lastName = "Shah"
+age = 31 
+jobRole = "Senior system integrator"
+company = "Arthrex"
+education = "Masters of Information Systems"
+print("We are super excited to announce that we have hired a new employee in our team. I would like to introduce " + firstName, lastName)
